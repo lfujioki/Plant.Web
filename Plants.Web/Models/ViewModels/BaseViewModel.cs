cdd62486d79b5c8c36f5a03163ed595c0734085e
Lauren Fujioki <laurenfujioki@gmail.com>
2016-04-11 15:31:05 -0700

    Folder for View models

---
+++ Plants.Web/Models/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plants.Web.Models.ViewModels
+{
+    public class BaseViewModel
+    {
+        public string PageTitle { get; set; }
+        public string UserName { get; set; }
+        public string ApplicationName { get; set; }
+    }
+}
---
+++ Plants.Web/Models/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plants.Web.Models.ViewModels
+{
+    public class ItemsViewModel<T, U> : BaseViewModel
+    {
+        public List<T> Items { get; set; }
+
+        public U Item { get; set; }
+    }
+}
